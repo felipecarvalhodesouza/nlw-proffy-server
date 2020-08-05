3cd171d698f380ae0af3053eb20b84ff71ce96b5
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2020-08-05 08:23:05 -0300

    Abstraindo a rotas rotas para um pacote separado

--- src/routes.ts
+++ src/routes.ts
@@ -1,71 +1,9 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHoursToMinutes';
+import ClassesController from './controllers/classesController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
-
-routes.post('/classes', async (request, response) => {
-    // desestruturação do js
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    // usado para fazer rollback em erros no mesmo contexto
-    const trx = await db.transaction();
-
-    try{
-        // Usando short syntax, que não funciona no IE
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        });
-
-        const user_id = insertedUsersIds[0];
-
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        })
-
-        const class_id = insertedClassesIds[0];
-
-        const classSchedule = schedule.map((scheduleItem:ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to)
-            };
-        })
-
-        await trx('class_schedule').insert(classSchedule);
-
-        //commit dos inserts
-        await trx.commit();
-
-        return response.status(201).send();
-
-    } catch(err){
-        await trx.rollback();
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
-    }
-});
+routes.post('/classes', classesController.create);
 
 export default routes;
