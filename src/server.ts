0aabf4f4f21a7a2877c31d7f689a09e609417b36
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2020-08-04 21:12:46 -0300

    Configurando o banco de dados Sqlite3 - Criando tabelas via migrations, e configurando o knex para fazer a leitura do typescript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+
+//Express não entende retorno da requisição em JSON por padrão.
+app.use(express.json())
+
+// Métodos HTTP
+// GET
+// POST
+// PUT
+// DELETE
+
+// Tipos de parâmetros
+// Corpo (ResquestBody) : Criação ou atualização  de um registro.
+// Route Params : Identificar qual recurso eu quero atualizar ou deletar.
+// Query Params : Paginação, filtros, ordenação, etc
+
+app.get('/', (request, response) => {
+    return response.json( {message: 'Hello World!'});
+});
+
+app.listen(3333);
+
+// KNEX serve para escrever queries SQL em Js;
+// SQLite 3 foi o banco selecionado.
+
